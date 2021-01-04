752eac556a5705041309a007a5f1c2d3dc215bf5
Kyle <kyle_derrick@foxmail.com>
2021-01-04 10:35:39 +0800

    update

--- code/esol_manage/src/plugins/element.js
+++
@@ -1,8 +0,0 @@
-import Vue from "vue";
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import "../assets/css/index.css";
-
-Vue.use(ElementUI);
-
-//ElementUI引入
--- code/esol_web/src/plugins/vant.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-import Vant from "vant";
-import "vant/lib/index.css";
-import "@vant/touch-emulator";
-import "../assets/css/index.css";
-
-Vue.use(Vant);
-
-//vant引入
